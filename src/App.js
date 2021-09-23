66a06b46ebcba2211ce35fade42064405e4d8684
vinay <vinayreddyin4@gmail.com>
2021-09-24 00:33:02 +0100

    test

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          App testing from dev changes  <code> react project</code> and save to reload.
+          App testing from dev changes  <code> react project</code>
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Instagram
         </a>
       </header>
     </div>
