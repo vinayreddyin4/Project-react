0eac01167b974a9edbc7526760cd4bc29aa7a6ba
Vinay <vinayreddyin4@gmail.com>
2022-05-18 18:52:10 +0100

    home changes

---
+++ src/Components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+
+class Home extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <div>
+                    <button> Men's</button>
+                    <p></p>
+                    <button> Women's</button>
+                    <div>
+                        <a href='https://aldoshoes.co.uk/collections/mid-season-sale-1'>Sales</a>
+                    </div>
+                    {/* <img src= "https://wallpaperaccess.com/full/4412757.jpg" width ="1450"/> */}
+
+                </div>
+            </div>
+        )
+    }
+}
+export default Home;
